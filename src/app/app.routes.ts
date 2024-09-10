e5edf4178c579834f3cc685c494cfeb7ce8599ca
GiftTeddy <serathiG02@gmail.com>
2024-09-10 23:39:55 +0200

    generated projects page and created routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/pages/home/home.component';
 import { AboutComponent } from '../app/pages/about/about.component';
-import { PortfolioComponent } from '../app/pages/portfolio/portfolio.component';
+import { ProjectsComponent } from '../app/pages/projects/projects.component';
 import { ContactComponent } from '../app/pages/contact/contact.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
